99a357d681f77918697df9cd129f69fb5deea606
JoyfulCuttlefish <113890367+JoyfulCuttlefish@users.noreply.github.com>
2024-02-19 20:27:39 +0100

    add stash display to index

--- lib/stash_web/controllers/stash_controller.ex
+++ lib/stash_web/controllers/stash_controller.ex
@@ -2,8 +2,13 @@ defmodule StashWeb.StashController do
   use StashWeb, :controller
 
 
-  def index(conn, _params) do
 
-    render(conn, :index, layout: false)
+  def index(conn, _params) do
+    stash = Stash.init()
+           |> Stash.put(:key, "value")
+           |> Stash.put(:toy, "car")
+           |> Stash.put(:hello, "greeting")
+
+    render(conn, :index, stash: stash)
   end
 end
