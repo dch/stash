57a62c7f4e47bce4a3b0a5e7355bef2c09daab07
Dave Cottlehuber <dch@skunkwerks.at>
2024-02-23 08:06:40 +0000

    mix: add tailwind dependencies

--- README.md
+++ README.md
@@ -7,11 +7,16 @@ A great place to keep your stuff.
 - install Elixir 1.15.7 or newer
 - ensure you have Erlang/OTP 25 or newer
 - ensure you have a local postgres server
+- install tailwind via npm if you are on FreeBSD
 
 ```shell
 $ git clone git@github.com:dch/stash
 $ cd ./stash
-$ mix setup
+$ test $(uname -s) = FreeBSD && \
+    npm install --prefix assets -D tailwindcss@latest postcss@latest autoprefixer@latest @tailwindcss/forms
+$ mkdir _build
+$ ln -s ../assets/node_modules/.bin/tailwindcss/tailwind _build/tailwind-freebsd-(uname -m)
+$ mix do setup, assets.deploy
 $ mix test --trace
 $ iex --name stash -S mix phx.server
 ```
--- mix.exs
+++ mix.exs
@@ -45,6 +45,14 @@ defmodule Stash.MixProject do
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
+      {:heroicons,
+       github: "tailwindlabs/heroicons",
+       tag: "v2.1.1",
+       sparse: "optimized",
+       app: false,
+       compile: false,
+       depth: 1},
       {:swoosh, "~> 1.5"},
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 0.6"},
@@ -68,9 +76,10 @@ defmodule Stash.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["esbuild.install --if-missing"],
-      "assets.build": ["esbuild stash"],
+      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.build": ["tailwind.stash", "esbuild stash"],
       "assets.deploy": [
+        "tailwind stash --minify",
         "esbuild stash --minify",
         "phx.digest"
       ]
